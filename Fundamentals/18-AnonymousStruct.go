c9b3138b75c875fb3373ac68b47c59c7079e8e00
Ra2ieh <razieh.naeemi@outlook.com>
2023-06-03 18:13:15 +0430

    ValueTypes and pointers

--- Fundamentals/18-AnonymousStruct.go
+++ Fundamentals/18-AnonymousStruct.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main() {
+	//literal type
 	p1 := struct {
 		first string
 		last  string
@@ -12,6 +13,13 @@ func main() {
 		last:  "Naeemi",
 		age:   30,
 	}
-
 	fmt.Println(p1)
+	//zero value
+	p2 := struct {
+		first string
+		last  string
+		age   int
+	}{}
+
+	fmt.Println(p2)
 }
